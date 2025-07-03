17adfd9a9c0a503b361acc32ab9d7b00e7d84778
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-03 21:31:16 +0545

    Initial project setup: Created spring boot project with web, jpa, and Mysql dependencies

---
+++ src/main/java/com/usermanagentsystem/usermanagementsystem/UserManagentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.usermanagentsystem.usermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usermanagentsystem/usermanagementsystem/UserManagentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usermanagentsystem.usermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
