0ecad01e64550347603ed696dbca50c7e36b5bc1
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-07-06 20:42:54 +0545

    Implement generate jwt token generation

--- src/main/java/com/usermanagementsystem/usermanagementsystem/config/JwtService.java
+++ src/main/java/com/usermanagementsystem/usermanagementsystem/config/JwtService.java
@@ -1,8 +1,27 @@
 package com.usermanagementsystem.usermanagementsystem.config;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
+
+    private final Key key= Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private final long EXPIRATION_TIME= 1000 * 60 * 60 * 24;
+
+    public String generateToken(String username){
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
+    }
 
 }
